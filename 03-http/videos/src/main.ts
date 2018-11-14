50193622d7b9efda185bb3663fd941db401729ae
KevinCanacuanPasquel <kfc0_10@hotmail.com>
2018-11-14 15:52:33 -0500

    Decoradores

--- 03-http/videos/src/main.ts
+++ 03-http/videos/src/main.ts
@@ -1,7 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+//cont NestFactory = require('@nestjs/core').NestFacory //js
+import { NestFactory } from '@nestjs/core'; //ts
+
+//import * as httserver from 'http-server'
+import {Options} from 'http-server';  //js
 import { AppModule } from './app.module';
 
+//const a = require('./mi-codigo').a    //js
+import {a} from "./mi-codigo";
+
 async function bootstrap() {
+  console.log(a);
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
