4d997a8e819ca2e28a441fecb2fb9c5fda99a8d0
KevinCanacuanPasquel <kfc0_10@hotmail.com>
2019-01-15 15:24:30 -0500

    Manejo de boostrap - interfaces web

--- 03-http/videos/src/main.ts
+++ 03-http/videos/src/main.ts
@@ -8,10 +8,34 @@ import { AppModule } from './app.module';
 //const a = require('./mi-codigo').a    //js
 import {a} from "./mi-codigo";
 
+//typescipt
+//import * as session from 'express-session';
+//node
+const session = require('express-session');
+
+const FileStore = require('session-file-store')(session);
+
+import * as express from 'express';
+
 async function bootstrap() {
-  console.log(a);
-  const app = await NestFactory.create(AppModule);
-  app.set('view engine', 'ejs');
-  await app.listen(3000);
+    console.log(a);
+    const app = await NestFactory.create(AppModule);
+    app.set('view engine', 'ejs');
+
+    app.use(
+        session({
+            secret: 'No sera de tomar un traguito',
+            resave: false,
+            saveUninitialized: true,
+            cookie: {secure: true}
+        })
+    );
+
+
+    //CONFIGURAR EL SERVIDOR WEB
+    app.use(express.static('publico'));
+
+    // @ts-ignore
+    await app.listen(3000);
 }
 bootstrap();
